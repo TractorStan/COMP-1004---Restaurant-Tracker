1e0631cb683903bbe0a4522b8120242543e4bed4
Jrussell <jakob.russell@students.plymouth.ac.uk>
2021-01-20 15:13:18 +0000

    On the way to actually being "single page"

---
+++ wwwroot/js/spa.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    $('#book').click(function () {
+        $('#page-content-wrapper').load('Book.html');
+    })
+});$(document).ready(function () {
+    $('#index').click(function () {
+        $('#page-content-wrapper').load('Index.html');
+    })
+});$(document).ready(function () {
+    $('#amend').click(function () {
+        $('#page-content-wrapper').load('Amend.html');
+    })
+});$(document).ready(function () {
+    $('#scan').click(function () {
+        $('#page-content-wrapper').load('ScanQR.html');
+    })
+});
