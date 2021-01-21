c0cbce6a54c97512b1340003515252609316d0c4
Jrussell <jakob.russell@students.plymouth.ac.uk>
2021-01-21 13:21:17 +0000

    SPA.js almost working

--- wwwroot/js/spa.js
+++ wwwroot/js/spa.js
@@ -1,17 +1,17 @@
-﻿$(document).ready(function () {
-    $('#book').click(function () {
-        $('#page-content-wrapper').load('Book.html');
-    })
-});$(document).ready(function () {
-    $('#index').click(function () {
-        $('#page-content-wrapper').load('Index.html');
-    })
-});$(document).ready(function () {
-    $('#amend').click(function () {
-        $('#page-content-wrapper').load('Amend.html');
-    })
-});$(document).ready(function () {
+﻿$(document).ready(function () {
     $('#scan').click(function () {
         $('#page-content-wrapper').load('ScanQR.html');
     })
-});
+});$(document).ready(function () {
+    $('#homepage').click(function () {
+        $('#page-content-wrapper').load('Homepage.html');
+    })
+});$(document).ready(function () {
+    $('#book').click(function () {
+        $('#page-content-wrapper').load('Book.html');
+    })
+});$(document).ready(function () {
+    $('#amend').click(function () {
+        $('#page-content-wrapper').load('Amend.html');
+    })
+});
